a605b99ce49e45a4e469f84d4ba6d32912fdcb28
Faisaljaved <faisaljaved06@gmail.com>
2017-05-22 17:40:04 +0500

    some add

---
+++ Assets/2D Platformer Controller/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	Transform target;
+	public float speed = 3f;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void Update () {
+		transform.position = Vector2.MoveTowards(transform.position,player.transform.position, speed*Time.deltaTime);
+	}
+}
